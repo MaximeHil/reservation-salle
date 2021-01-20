5151535aea1d135cf49c48ac628be16f7e892d09
unknown <maxime.hilberer.etu@univ-lille.fr>
2021-01-20 11:34:44 +0100

    Changement de la liste en card deck

--- reservation-salle-front/src/views/Home.vue
+++ reservation-salle-front/src/views/Home.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="home">
+
     <div class="input-group">
-      <input type="text" v-model="nameFilter" class="form-control" aria-label="" style="margin-left: 4%; margin-right: 1%">
+      <input type="text" v-model="nameFilter"
+             @keyup.enter="setRoomsFilteredByName(nameFilter)"
+             class="form-control" aria-label=""
+             style="margin-left: 4%; margin-right: 1%">
       <div class="input-group-append" style="margin-right: 4%">
         <button class="btn btn-secondary" @click="setRoomsFilteredByName(nameFilter)">Filter by name</button>
       </div>
-      <input type="text" v-model="equipementFilter" class="form-control" aria-label="" style="margin-left: 4%; margin-right: 1%">
+      <input type="text" v-model="equipementFilter"
+             @keyup.enter="setRoomsFilteredByEquipement(equipementFilter)"
+             class="form-control" aria-label=""
+             style="margin-left: 4%; margin-right: 1%">
       <div class="input-group-append" style="margin-right: 4%">
-        <button class="btn btn-secondary" @click="setRoomsFilteredByEquipement(equipementFilter)">Filter by equipment</button>
+        <button class="btn btn-secondary test" @click="setRoomsFilteredByEquipement(equipementFilter)">Filter by equipment</button>
       </div>
     </div>
-    <ul class="list-group">
+    <div class="card-deck">
+      <RoomCardItem v-for="room in rooms"
+                    :key="room.name"
+                    :name="room.name"
+                    :description="room.description"
+                    :capacity="room.capacity"
+                    :equipements="room.equipements"/>
+    </div>
+    <!--<ul class="list-group">
     <RoomItem v-for="room in rooms"
     :key="room.name"
     :name="room.name"
     :description="room.description"
     :capacity="room.capacity"
     :equipements="room.equipements"/>
-    </ul>
+    </ul>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapGetters, mapActions } from 'vuex'
-import RoomItem from "@/components/RoomItem";
-
+//import RoomItem from "@/components/RoomItem";
+import RoomCardItem from "@/components/RoomCardItem";
 export default {
   name: 'Home',
 
@@ -36,7 +51,8 @@ export default {
     }
   },
   components: {
-    RoomItem,
+    //RoomItem,
+    RoomCardItem
   },
   methods: {
       ...mapActions(['setAllRooms', 'setRoomsFilteredByName', 'setRoomsFilteredByEquipement']),
@@ -52,8 +68,12 @@ export default {
 </script>
 
 <style scoped>
-.list-group {
+.card-deck {
   margin-top: 2%;
+}
+
+.test{
+  border-radius: 5px;
 }
 
 </style>
