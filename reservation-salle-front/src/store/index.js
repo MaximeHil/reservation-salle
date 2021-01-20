9d64f873a8d95deef24f61c206a31508c8dbd400
unknown <maxime.hilberer.etu@univ-lille.fr>
2021-01-20 10:24:00 +0100

    Ajout du filtrage par nom/equipement

--- reservation-salle-front/src/store/index.js
+++ reservation-salle-front/src/store/index.js
@@ -1,78 +1,55 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
     rooms:[
-      {
-        "name":"Salle #1",
-        "description":"Salle #1",
-        "capacity":5,
-        "equipements":[
-          {
-            "name":"TV"
-          },
-          {
-            "name":"Retro Projecteur"
-          }
-        ],
-        "createdAt":"2016-12-07T12:39:29.812Z",
-        "updatedAt":"2016-12-08T17:31:39.489Z"
-      },
-      {
-        "name":"Salle #2",
-        "description":"Salle #2",
-        "capacity":10,
-        "equipements":[
-          {
-            "name":"Retro Projecteur"
-          }
-        ],
-        "createdAt":"2016-12-07T12:39:55.384Z",
-        "updatedAt":"2016-12-07T13:33:37.184Z"
-      },
-      {
-        "name":"Salle Okjsdkso",
-        "description":"Salle Okjsdkso",
-        "capacity":11,
-        "equipements":[],
-        "createdAt":"2016-12-07T14:15:55.733Z",
-        "updatedAt":"2016-12-09T16:45:19.025Z"
-      },
-      {
-        "name":"Salle de ouf",
-        "description":"Salle de ouf",
-        "capacity":10,
-        "equipements":[
-          {
-            name:"TV"
-          },
-          {
-            "name":"Retro Projecteur"
-          }
-        ],
-        "createdAt":"2016-12-09T16:45:34.419Z",
-        "updatedAt":"2016-12-09T16:45:34.419Z"
-      },
-      {
-        "name":"Salle nulle",
-        "description":"Salle nulle",
-        "capacity":26,
-        "equipements":[
-          {
-            "name":"TV"
-          },
-          {
-            "name":"Retro Projecteur"
-          }
-        ],
-        "createdAt":"2016-12-09T16:45:49.096Z",
-        "updatedAt":"2016-12-09T16:45:49.096Z"
-      }
+
     ]
   },
+  getters: {
+    rooms(state) {
+      return state.rooms;
+    }
+  },
   mutations: {
+    SET_ROOMS: (state, rooms) => {
+      console.log(rooms);
+      while(state.rooms.length > 0) {
+        state.rooms.pop();
+      }
+      state.rooms.push(...rooms);
+    }
   },
   actions: {
+    setAllRooms({ commit }) {
+      axios.get("http://localhost:3000/rooms/")
+          .then(response => commit('SET_ROOMS', response.data.rooms));
+    },
+
+    setRoomsFilteredByName({ commit }, filter){
+      console.log("Dans le filter by name \n");
+      console.log(filter);
+      let url = 'http://localhost:3000/rooms'
+      const filterTrim =  filter.trim();
+      if(filterTrim.length){
+        url += '/name/'
+      }
+      axios.get( url + filterTrim)
+          .then(response => commit('SET_ROOMS', response.data.rooms));
+    },
+
+    setRoomsFilteredByEquipement({ commit }, filter){
+      console.log("Dans le filter by equipment \n");
+      console.log(filter);
+      let url = 'http://localhost:3000/rooms'
+      const filterTrim =  filter.trim();
+      if(filterTrim.length){
+        url += '/equipement/'
+      }
+      axios.get( url + filterTrim)
+          .then(response => commit('SET_ROOMS', response.data.rooms));
+    }
   },
   modules: {
   }
--- reservation-salle-front/src/views/Home.vue
+++ reservation-salle-front/src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="home">
+    <div class="input-group">
+      <input type="text" v-model="nameFilter" class="form-control" aria-label="" style="margin-left: 4%; margin-right: 1%">
+      <div class="input-group-append" style="margin-right: 4%">
+        <button class="btn btn-secondary" @click="setRoomsFilteredByName(nameFilter)">Filter by name</button>
+      </div>
+      <input type="text" v-model="equipementFilter" class="form-control" aria-label="" style="margin-left: 4%; margin-right: 1%">
+      <div class="input-group-append" style="margin-right: 4%">
+        <button class="btn btn-secondary" @click="setRoomsFilteredByEquipement(equipementFilter)">Filter by equipment</button>
+      </div>
+    </div>
     <ul class="list-group">
     <RoomItem v-for="room in rooms"
     :key="room.name"
@@ -13,16 +23,37 @@
 
 <script>
 // @ is an alias to /src
-import { mapState } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import RoomItem from "@/components/RoomItem";
 
 export default {
   name: 'Home',
+
+  data: function () {
+    return {
+      nameFilter: '',
+      equipementFilter: '',
+    }
+  },
   components: {
     RoomItem,
   },
+  methods: {
+      ...mapActions(['setAllRooms', 'setRoomsFilteredByName', 'setRoomsFilteredByEquipement']),
+
+  },
   computed: {
-    ...mapState(['rooms'])
-  }
+    ...mapGetters(['rooms'])
+  },
+  mounted() {
+    this.setAllRooms();
+  },
 }
 </script>
+
+<style scoped>
+.list-group {
+  margin-top: 2%;
+}
+
+</style>
